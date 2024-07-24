5c65080dccda7d666912dd346f621d4cf71155e8
leucimarAntonio <leu.cimarjr@gmail.com>
2024-07-24 00:37:11 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+let xp = 7850
+let nomeHeroi = "Erica";
+let nivel ="";
+if (xp <= 1000 ){
+nivel ="Ferro";  
+} else if (xp >=1001 && xp <=2000){
+nivel ="Bronze";
+} else if (xp >= 2001 && xp <= 5000){
+nivel ="Prata";
+} else if (xp >= 5001 && xp <= 7000){
+nivel ="Ouro";
+} else if(xp >= 7001 && xp <= 8000){
+nivel="Platina";
+} else if(xp >= 8001 && xp <= 9000){
+nivel="Ascendente";
+} else if(xp >=9001 && xp <= 10000){
+nivel="Imortal";
+} else {
+nivel="Radiante";
+}
+
+console.log("O Heroi " + nomeHeroi + " Esta no nivel " + nivel);
+console.log(`o Heroi ${nomeHeroi} esta no nivel ${nivel} com o "xp" ${xp}`)
